5ee64a0857cb0bdc05885ed2113aae45d452fd55
Serginho2405 <148503570+Serginho2405@users.noreply.github.com>
2023-11-30 23:20:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cells = document.querySelectorAll('.available, .unavailable');
+    cells.forEach(cell => {
+        cell.addEventListener('click', toggleAvailability);
+    });
+});
+
+function toggleAvailability() {
+    this.classList.toggle('available');
+    this.classList.toggle('unavailable');
+}
+document.addEventListener("DOMContentLoaded", function () {
+    const cells = document.querySelectorAll('.available, .unavailable');
+    cells.forEach(cell => {
+        cell.addEventListener('click', toggleAvailability);
+    });
+});
+
+function toggleAvailability() {
+    const previousStatus = this.classList.contains('available') ? 'Disponível' : 'Indisponível';
+    
+    this.classList.toggle('available');
+    this.classList.toggle('unavailable');
+
+    const newStatus = this.classList.contains('available') ? 'Disponível' : 'Indisponível';
+
+    alert(`Disponibilidade alterada de ${previousStatus} para ${newStatus}`);
+}
